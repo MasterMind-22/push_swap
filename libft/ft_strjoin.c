265f79f083cb76bf9f9da970cd614040694b9576
Youssef Nadry <115162045+MasterMind-22@users.noreply.github.com>
2023-03-26 16:07:41 +0000

    backtracking fixed

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: yonadry <yonadry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:44:20 by yonadry           #+#    #+#             */
-/*   Updated: 2023/03/25 19:31:47 by yonadry          ###   ########.fr       */
+/*   Updated: 2023/03/26 16:03:31 by yonadry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str2 = (char *)s2;
 	if (!str1)
 		str1 = ft_strdup("");
-	if (str2)
+	if (!str2)
 		return (NULL);
 	res = malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
 	if (!res)
