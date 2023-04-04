bc0a5fbad6b81b216e524d8b91f5a9dd1dbbf723
Youssef Nadry <115162045+MasterMind-22@users.noreply.github.com>
2023-04-04 23:55:09 +0000

    find longest

--- sort_number.c
+++ sort_number.c
@@ -19,26 +19,57 @@
  	}
  }
 
+int longest_inc_sub(t_list **stack_a)
+{
+	t_list *head = (*stack_a);
+	t_list *head1 = *stack_a;
+	t_list *tmp = *stack_a;
 
+	int i = ft_lstsize(head);
+	int longg = 1;
+	int index;
+	while (i--)
+	{
+		index = 1;
+		while (head->next)
+		{
+			if (head1->content < head->content)
+			{
+				index++;
+				// head1 = head;
+				printf("++++++| %d |++++++\n", head1->content);
+			}
+			head = head->next;
+		}
+		if (index > longg)
+			longg = index;
+		// printf("<<<<<< %d >>>>>\n", head1->content);
+		head = head1->next;
+		head1 = head1->next;
+	}
+		// printf("%d\n", longg);
+	return longg;
+}
 
 void sort_3_nums(t_list **stack_a,	t_list **stack_b)
 {
-	t_list *head = (*stack_a);
-	index_list(stack_a);
-	int i = ft_lstsize(*stack_a);
-	while (i--)
-	{
-		head = *stack_a;
-		while ((*stack_a)->next)
-		{
-			if ((*stack_a)->index > (*stack_a)->next->index)
-				pb(stack_a, stack_b);
-			else if ((*stack_a)->index < (*stack_a)->next->index)
-			{
-				ra(stack_a, 1);
-				// break;
-			}
-			(*stack_a) = (*stack_a)->next;
-		}
-	}
+	longest_inc_sub(stack_a);
+	// t_list *head = (*stack_a);
+	// index_list(stack_a);
+	// int i = ft_lstsize(*stack_a);
+	// while (i--)
+	// {
+	// 	head = *stack_a;
+	// 	while (head)
+	// 	{
+	// 		if (head->index > head->next->index)
+	// 			ra(stack_a, 1);
+	// 		else
+	// 		{
+	// 			ra(stack_a, 1);
+	// 			break;
+	// 		}
+	// 		head = head->next;
+	// 	}
+	// }
 }
